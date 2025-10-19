db6c7abd6822dbe8c07b239bfa27ecb9d17d3a5e
Isabella Jones <injones@usf.edu>
2025-10-19 19:53:57 -0400

    Initial Commit with .fetchProductAsync() added onto JS

--- cp_2.js
+++ cp_2.js
@@ -1,3 +1,4 @@
+// Fetching the data to use in the product dashboard
 function fetchProductsThen() {
   fetch("https://www.course-api.com/javascript-store-products") //The provided link for the products
     .then(response => {
@@ -14,4 +15,18 @@ function fetchProductsThen() {
     .catch(error => { // here to log any fetched errors
       console.error("Fetch error in fetchProductsThen():", error.message)
     })
+}
+
+// Calls upon the data and checks if the fetch fails
+async function fetchProductsAsync() {
+  try {
+    const response = await fetch("https://www.course-api.com/javascript-store-products"); //same link given
+    if (!response.ok) {
+      throw new Error(`HTTP Error!! Status: ${response.status}`)
+    }
+    const products = await response.json()
+    displayProducts(products)
+  } catch (error) {
+    handleError(error)
+  }
 }
